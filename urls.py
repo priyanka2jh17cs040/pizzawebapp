28ef8d38f3f59861b77e755c5c53a2687e158602
PRIYANKA_M_HATAPAKKI <pmhatapakki1999@gmail.com>
2020-07-26 00:41:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,17 +1,22 @@
+"""pizza URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('register', views.register, name='register'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('add_item', views.addItem_view, name='add_item'),
-    path('remove_item/<int:cart_item_id>', views.removeItem_view, name='remove_item'),
-    path('empty_cart', views.emptyCart_view, name='empty_cart'),
-    path('cart', views.cart_view, name='cart'),
-    path('order', views.order_view, name='order'),
-    path('orders', views.orders_view, name='orders'),
-    path('vieworders', views.viewOrders_view, name='vieworders'),
-    path('markComplete/<int:order_item_id>', views.markComplete_view, name='markcomplete')
+    path('', include('orders.urls')),
+    path('admin/', admin.site.urls),
 ]
